7385c189840b6dcd9eadde3eb77807b32e0a6a2b
liawesomesaucer <vincent.oddrepublic@gmail.com>
2018-02-02 14:53:55 -0800

    Add hot reload with browserSync

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # A static site boilerplate
-A boilerplate for static site
+A boilerplate for static site. Includes SASS and hot-reload (html & sass/css) through browserSync
 
-## How to build SASS
-Since this is static, we must compile the SASS, so run this:
+## Setup
+Run `npm install` to get all dependencies for this boilerplate
+
+## How to build
+Since this is static, we must compile the SASS and hot reload, so run this:
 
 ```
-npm install
 gulp
 ```
+
+BrowserSync should open a tab for you but if it doesn't, navigate to `http://localhost:3000`.
--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,31 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var ecstatic = require('ecstatic');
+var http = require('http');
+var browserSync = require('browser-sync').create();
 
 var paths = {
   scss: ['scss/main.scss'],
   css: 'css/'
 };
+
+gulp.task('serve', function(){
+  http.createServer(
+    ecstatic({ root: __dirname })
+  ).listen(8000);
+
+  console.log('Listening on :8000');
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 gulp.task('build-sass', function() {
   return gulp.src(paths.scss)
@@ -17,11 +37,14 @@ gulp.task('build-sass', function() {
       cascade: false
     }))
     .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest(paths.css));
+    .pipe(gulp.dest(paths.css))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
   gulp.watch('scss/**/*.scss', ['build-sass']);
+  gulp.watch('*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['watch', 'build-sass']);
+gulp.task('default', ['watch', 'build-sass', 'serve', 'browser-sync']);
+
